3df3b7b47664dc2ccd9822d8b494d25b66df31b1
Greg <greg@smartsoftasia.com>
2017-07-12 19:29:41 +0700

    init commit

---
+++ backend/src/main/kotlin/com/smartsoftasia/BusinessCard/BusinessCardApplication.kt
@@ -0,0 +1,11 @@
+package com.smartsoftasia.BusinessCard
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BusinessCardApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BusinessCardApplication::class.java, *args)
+}
---
+++ backend/src/test/kotlin/com/smartsoftasia/BusinessCard/BusinessCardApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.smartsoftasia.BusinessCard
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BusinessCardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
